5a64b310e9bcce64b465a40071e7e15fba7ddbc9
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-02-13 23:36:24 +0530

    Add function operate to do mathematical operations
    Created variables to firstNum secondNum operator to store two numbers and an operator and created function operate() to evaluate the result

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function multiply (a, b){
 function divide (a, b){
     return a / b;
 };
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate (firstNum, operator, secondNum){
+    if (operator == '+') return add(firstNum, secondNum);
+    if (operator == '-') return subtract(firstNum, secondNum);
+    if (operator == '*') return multiply(firstNum, secondNum);
+    if (operator == '/') return divide(firstNum, secondNum);
+};
