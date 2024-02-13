a24c4fd0c59f527fdd323159a3ff893f481d25ff
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-02-14 01:45:33 +0530

    Add function that shows value of clicked button
    Added function showOnDisplay that shows value of the buttons which is being clicked on the display div and stores it in variable number and sign

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ function operate (firstNum, operator, secondNum){
     if (operator == '*') return multiply(firstNum, secondNum);
     if (operator == '/') return divide(firstNum, secondNum);
 };
+
+
+let display = document.querySelector('#display');
+
+function showOnDisplay(){
+    let signs = document.querySelectorAll('.sign');
+    for(let i = 0; i < signs.length; i++){
+        let sign;
+        signs[i].addEventListener('click', () => {display.textContent = signs[i].value;
+        return sign = signs[i].value})
+    }
+
+    let numbers = document.querySelectorAll('.numbers');
+    for (let i = 0; i < numbers.length; i++){
+        let number;
+        numbers[i].addEventListener('click', () => {display.textContent = numbers[i].value;
+        return number = numbers[i].value})
+    }
+};
+
+showOnDisplay();
