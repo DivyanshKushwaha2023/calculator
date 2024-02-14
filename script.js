de46054f0c6dfb9f9c1a8a50bdbaf602e6e571a6
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-02-14 22:21:41 +0530

    Delete function showOnDisplay & getOperator
    Deleted showOnDisplay and getOperator , they were not working

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ function divide (a, b){
     return a / b;
 };
 
-let firstNum;
-let secondNum;
-let operator;
 
 function operate (firstNum, operator, secondNum){
     if (operator == '+') return add(firstNum, secondNum);
@@ -26,26 +23,3 @@ function operate (firstNum, operator, secondNum){
 };
 
 
-let display = document.querySelector('#display');
-let number;
-function showOnDisplay(){
-    let numbers = document.querySelectorAll('.numbers');
-    for (let i = 0; i < numbers.length; i++){
-
-        numbers[i].addEventListener('click', () => {display.textContent = numbers[i].value;
-        number = numbers[i].value;})
-    }
-};
-
-showOnDisplay();
-
-function getOperator(){
-    let signs = document.querySelectorAll('.sign');
-    for(let i = 0; i < signs.length; i++){
-        signs[i].addEventListener('click', () =>{
-            operator = signs[i].value;
-        });
-    }
-};
-
-getOperator();
