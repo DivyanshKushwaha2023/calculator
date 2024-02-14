66eb89a9ba0b7910b1027ac4897a4348c8fedfe3
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-02-14 23:08:43 +0530

    Add displayIt() that displays numbers on screen
    Add function that displays digits for number buttons clicked and stores them in number array

--- script.js
+++ script.js
@@ -24,4 +24,34 @@ function operate (firstNum, operator, secondNum){
 
 let display = document.querySelector('#display')
 let clear = document.querySelector('#clear');
-clear.addEventListener('click', () => display.textContent = null)
+clear.addEventListener('click', () => clearIt());
+
+function clearIt(){
+    display.textContent = null;
+}
+
+
+let numbers = document.querySelectorAll('.numbers');
+let signs = document.querySelectorAll('.sign');
+
+function displayIt(){
+    let number = [];    
+    for (let i = 0; i < numbers.length; i++){
+        numbers[i].addEventListener('click', () =>{
+            display.innerHTML += numbers[i].value;
+            number.push(numbers[i].value);
+        });
+    };
+
+    for(let i = 0; i < signs.length; i++){
+        signs[i].addEventListener('click', () =>{
+            clearIt();
+            console.log(number);
+            number = [];
+            
+        })
+    }
+};
+
+displayIt();
+
